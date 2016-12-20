492672d1bdc93bf6f18869a5fa5dfa0737b5a62f
Evgeny <st054504@student.spbu.ru>
2016-12-20 23:06:57 +0300

    Create Sorts.h

---
+++ Sorts.h
@@ -0,0 +1,8 @@
+#pragma once
+
+int comparator(char *x, char *y);
+void quicksort(char **str, int size);
+void bubblesort(char **str, int size);
+void insertion(char **str, int size);
+void mergesort(char **str, int n, int m);
+void merge(char **arr, int n, int j, int m);
